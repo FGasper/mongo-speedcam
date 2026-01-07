5b7d22b50f518fdeb48f40690260a2dea675bf62
Felipe Gasper <felipe@felipegasper.com>
2026-01-06 20:01:06 -0500

    more efficient cluster time parsing

--- cluster_time.go
+++ cluster_time.go
@@ -8,17 +8,14 @@ import (
 )
 
 func GetClusterTimeFromSession(sess *mongo.Session) (bson.Timestamp, error) {
-	ctStruct := struct {
-		ClusterTime struct {
-			ClusterTime bson.Timestamp `bson:"clusterTime"`
-		} `bson:"$clusterTime"`
-	}{}
+	ctRaw := sess.ClusterTime()
 
-	clusterTimeRaw := sess.ClusterTime()
-	err := bson.Unmarshal(sess.ClusterTime(), &ctStruct)
+	rv, err := ctRaw.LookupErr("$clusterTime", "clusterTime")
 	if err != nil {
-		return bson.Timestamp{}, fmt.Errorf("finding clusterTime in session cluster time document (%v): %w", clusterTimeRaw, err)
+		return bson.Timestamp{}, fmt.Errorf("finding clusterTime in session cluster time document (%v): %w", ctRaw, err)
 	}
 
-	return ctStruct.ClusterTime.ClusterTime, nil
+	t, i := rv.Timestamp()
+
+	return bson.Timestamp{t, i}, nil
 }
